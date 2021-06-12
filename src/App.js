e4d754cfa4eea9f4b9a87ef955b386bf4e741e28
adis-tek <67308354+adis-tek@users.noreply.github.com>
2021-06-11 23:16:37 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+    <Switch>
+      <h1>Initial Commit</h1>
+    </Switch>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
