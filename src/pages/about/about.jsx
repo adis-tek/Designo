09aab54a446b0944be40e4a0d57a732b9fa1c983
adis-tek <67308354+adis-tek@users.noreply.github.com>
2021-09-22 00:21:47 -0500

    fixed clipping content around nav bar

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -115,7 +115,7 @@ function about() {
             Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every
             opportunity. We make design and technology more accessible and give you tools to measure success.</p>
             <p className="talent-description">We are visual storytellers in appealing and captivating ways. By combining business and marketing strategies, 
-we inspire audiences to take action and drive real results.</p>
+            we inspire audiences to take action and drive real results.</p>
         </div>
     </div>
 </div>
